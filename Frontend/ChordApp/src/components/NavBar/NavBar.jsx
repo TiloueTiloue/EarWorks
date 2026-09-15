8a64ccd6e818fdec182041b7e6319ff48482570a
TiloueeeTiloueee <lp.leblanc@hotmail.com>
2026-09-14 20:26:47 -0400

    Enhanced home screen experience and design

--- Frontend/ChordApp/src/components/NavBar/NavBar.jsx
+++ Frontend/ChordApp/src/components/NavBar/NavBar.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import "./NavBar.css"
-import logo from "../../assets/musicnote.png"
+
 
 function NavBar() {
   return (
     <div className='nav-body'>
       <div className='nav-header'>
-        <Link className='logo-wrapper' to="/">
-          <img className='logo' src={logo} alt="app logo" />
+        <Link className='logo-wrapper' to='/'>
+          <img className='logo' src='' alt=''app logo />
         </Link>
         <p className='title'>EarWorks</p>
       </div>
       <div className='nav-content'>
-        <Link className='content-link' to="/notes">
+        <Link className='content-link' to='/notes'>
           <p>Notes</p>
         </Link>
-        <Link className='content-link' to="/chords">
+        <Link className='content-link' to='/chords'>
           <p>Chords</p>
         </Link>
-        <Link className='content-link' to="/scales">
+        <Link className='content-link' to='/scales'>
           <p>Scales</p>
         </Link>
-        <Link className='content-link' to="/keys">
+        <Link className='content-link' to='/keys'>
           <p>Keys</p>
         </Link>
       </div>
--- Frontend/ChordApp/src/pages/Home/Home.jsx
+++ Frontend/ChordApp/src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import notesImg from "../../assets/notes_icon.png"
+import chordsImg from "../../assets/chords_icon.png"
+import keysImg from "../../assets/keys_icon.png"
+import scalesImg from "../../assets/scales_icon.png"
 import "./home.css"
 import "../pages.css"
-
+//
 const Home = () => {
     const navigate = useNavigate();
 
@@ -26,20 +30,43 @@ const Home = () => {
 
     return (
         <div className="home">
-            <div className='welcome'>
-                <h1>Welcome to Ear Practice!</h1>
+            <div className='welcome-section'>
+                <h1>Welcome to EarWorks!</h1>
             </div>
             
+            <div className='about-section'>
+                <div className='about-text'>
+                    <h2>ABOUT</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac justo id quam elementum malesuada at ut odio. Donec nisi purus, eleifend ut aliquet quis, facilisis in velit. Cras egestas magna nisi, at semper sem rutrum et. Ut commodo eu enim ut sagittis. Suspendisse sapien velit, consectetur et imperdiet ac, suscipit quis nisi. Praesent posuere a dui in aliquet. Aliquam scelerisque, nunc eu tristique mollis, nisi dolor congue turpis, nec egestas lacus sem vehicula erat. Phasellus rutrum eros vitae accumsan sodales. Sed tristique lobortis lectus nec malesuada. Maecenas sed purus imperdiet, dictum leo quis, efficitur sem.</p>
+                </div>
+                <div className='about-text'>
+                    <h2>WHY</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac justo id quam elementum malesuada at ut odio. Donec nisi purus, eleifend ut aliquet quis, facilisis in velit. Cras egestas magna nisi, at semper sem rutrum et. Ut commodo eu enim ut sagittis. Suspendisse sapien velit, consectetur et imperdiet ac, suscipit quis nisi. Praesent posuere a dui in aliquet. Aliquam scelerisque, nunc eu tristique mollis, nisi dolor congue turpis, nec egestas lacus sem vehicula erat. Phasellus rutrum eros vitae accumsan sodales. Sed tristique lobortis lectus nec malesuada. Maecenas sed purus imperdiet, dictum leo quis, efficitur sem.</p>
+                </div>
+            </div>
+
             <div className='nav-section'>
                 <h3>What are we practicing today?</h3>
                 
                 <div className='nav-buttons'>
-                    <button onClick={() => handleNavigation('notes')}>Notes</button>
-                    <button onClick={() => handleNavigation('chords')}>Chords</button>
-                    <button onClick={() => handleNavigation('keys')}>Keys</button>
-                    <button onClick={() => handleNavigation('scales')}>Scales</button>
+                    <button onClick={() => handleNavigation('notes')}>
+                        <img src={notesImg}/>
+                        <span>Notes</span>
+                    </button>
+                    <button onClick={() => handleNavigation('chords')}>
+                        <img src={chordsImg}/>
+                        <span>Chords</span>
+                    </button>
+                    <button onClick={() => handleNavigation('keys')}>
+                        <img src={keysImg}/>
+                        <span>Keys</span>
+                    </button>
+                    <button onClick={() => handleNavigation('scales')}>
+                        <img src={scalesImg}/>
+                        <span>Scales</span>
+                    </button>
                 </div>
-
+                
             </div>
         </div>
     )
